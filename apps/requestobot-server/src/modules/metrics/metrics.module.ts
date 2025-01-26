153ef9f6750f1b5ec34fbf725da643e9e095449f
steglasaurous <me@steglasaurous.com>
2025-01-26 18:50:32 -0500

    WIP

--- apps/requestobot-server/src/modules/metrics/metrics.module.ts
+++ apps/requestobot-server/src/modules/metrics/metrics.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 
 @Module({
-  imports: [PrometheusModule.register()],
+  imports: [PrometheusModule.register({ customMetricPrefix: 'requestobot' })],
   exports: [PrometheusModule],
 })
 export class MetricsModule {}
