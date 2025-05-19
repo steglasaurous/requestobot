c554a66f9824d88b058bf67297b8b0503cb8b351
Steglasaurous <me@steglasaurous.com>
2025-05-19 15:22:04 -0400

    feat: functional overlay of playing youtube audio

---
+++ apps/requestobot-overlay/src/app/+state/song-player.actions.ts
@@ -0,0 +1,15 @@
+import { createActionGroup, emptyProps, props } from '@ngrx/store';
+import { SongDto } from '@requestobot/util-dto';
+
+export const SongPlayerActions = createActionGroup({
+  source: 'SongPlayer',
+  events: {
+    SetSongAndPlay: props<{ song: SongDto }>(),
+    Play: emptyProps(),
+    Pause: emptyProps(),
+    Stop: emptyProps(),
+    ChangePosition: props<{
+      change: number;
+    }>(),
+  },
+});
---
+++ apps/requestobot-overlay/src/app/+state/song-player.reducer.ts
@@ -0,0 +1,28 @@
+import { SongDto } from '@requestobot/util-dto';
+import { createReducer, on } from '@ngrx/store';
+import { SongPlayerActions } from './song-player.actions';
+
+export interface SongPlayerState {
+  song: SongDto | null;
+  // FIXME: I'd prefer to use the PlayerState defined in the backend, but typeorm gets funny about dependencies outside of
+  //   the server app.  (can't resolve library references I think).  If I figure that out some time, replace this.
+  playerState: string;
+}
+
+export const initialState: SongPlayerState = {
+  song: null,
+  playerState: 'stopped',
+};
+
+export const songPlayerReducer = createReducer(
+  initialState,
+  on(SongPlayerActions.setSongAndPlay, (state, { song }) => {
+    return { ...state, song: song, playerState: 'playing' };
+  }),
+  on(SongPlayerActions.pause, (state) => {
+    return { ...state, playerState: 'paused' };
+  }),
+  on(SongPlayerActions.stop, (state) => {
+    return { ...state, playerState: 'stopped' };
+  })
+);
